42ef3864c0424f3252dd24d2d4cf36b8045ad92c
LevinithD <39787294+LevinithD@users.noreply.github.com>
2020-12-17 08:43:18 +0100

    Rename ServiceDeskToolKit2.0/HelperFiles/HelpFile_Example.ps1 to ServiceDeskToolKit2.0/HelperFiles/Function_Files/General_Files/HelpFile_Example.ps1

---
+++ ServiceDeskToolKit2.0/HelperFiles/Function_Files/General_Files/HelpFile_Example.ps1
@@ -0,0 +1,15 @@
+﻿<#
+.SYNOPSIS
+
+.DESCRIPTION
+
+.PARAMETER
+
+.EXAMPLE
+
+.NOTES
+
+.LINK
+#>
+
+#DOT source library: Hier worden andere bestanden ingeladen die nodig zijn voor de werking van de ServiceDesk Toolkit 2.0
