6fffb7f8a4273ddbe8dbc13874b132696c66ef54
LevinithD <39787294+LevinithD@users.noreply.github.com>
2020-12-17 08:40:33 +0100

    Add files via upload

---
+++ ServiceDeskToolKit2.0/HelperFiles/StartPrograms.ps1
@@ -0,0 +1,14 @@
+﻿$ProgramsArray = "iexplore.exe", 
+                 "firefox.exe", 
+                 "mRemoteNG.exe", 
+                 "Outlook.exe", 
+                 "ServiceDesk Toolkit.exe"
+
+Foreach ($Program in $ProgramArray)
+{
+    If (!(Get-Process $Program))
+    {
+        Start-Process $Program
+        Write-Host $Program + " is running" 
+    }
+}
