10a08bc92e53c6ecc1cb6c8a07ee871eee998802
LevinithD <39787294+LevinithD@users.noreply.github.com>
2021-02-08 11:37:19 +0100

    Aanpassing van naam

---
+++ ServiceDeskToolKit2.0/HelperFiles/Function_Files/User_Files/Get-SamAccountName.ps1
@@ -0,0 +1,24 @@
+﻿Function Get-SamAccountName
+{
+    Param(
+         $Initials,
+         $Surname
+         )
+
+        If ($Initials.Length -gt 2)
+        {
+           # Hier moet per 2-tal karakters gecontroleerd worden of er karakter-punt-karakter-punt staat in een loop
+        }
+        Else
+        {
+            If ($Initials -notmatch '\.$')
+                {
+                    $Initials += '.'
+                }
+        }
+         
+        Get-ADUSER -Filter {Initials -like $Initials -and sn -like $Surname} | Select-Object SamAccountName
+    
+}
+
+Get-SamAccountName -Initials "Y" -Surname "Bool"
