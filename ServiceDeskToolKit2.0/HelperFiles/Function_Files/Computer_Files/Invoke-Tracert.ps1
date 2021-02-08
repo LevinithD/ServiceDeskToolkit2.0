dfa315bb4fb57ed767e06e89112ee8db54020c15
LevinithD <39787294+LevinithD@users.noreply.github.com>
2021-02-08 17:24:50 +0100

    Create Invoke-Tracert.ps1

---
+++ ServiceDeskToolKit2.0/HelperFiles/Function_Files/Computer_Files/Invoke-Tracert.ps1
@@ -0,0 +1,46 @@
+﻿Function Invoke-Tracert
+{
+    [CmdLetBinding()]
+    param(
+          $address
+         )
+
+    $ping = new-object System.Net.NetworkInformation.Ping
+    $timeout = 5000
+    $maxttl  = 64
+    
+    $message = [System.Text.Encoding]::Default.GetBytes("MESSAGE")
+    $dontfragment = $false
+    $success = [System.Net.NetworkInformation.IPStatus]::Success
+
+    echo "Tracing $address"
+    for ($ttl=1;$i -le $maxttl; $ttl++) 
+    {
+        $popt = new-object System.Net.NetworkInformation.PingOptions($ttl, $dontfragment)   
+        $reply = $ping.Send($address, $timeout, $message, $popt)
+
+
+        $addr = $reply.Address
+        $rtt = $reply.RoundtripTime
+
+        try 
+        {
+            $dns = [System.Net.Dns]::GetHostByAddress($addr)
+        } 
+        catch 
+        {
+            $dns = "-"
+        }
+
+        $name = $dns.HostName
+    
+        echo "Hop: $ttl`t= $addr`t($name)"
+        
+        if($reply.Status -eq $success) 
+        {
+            break
+        }
+    }
+}
+
+Mid-Waged-Mans-Tracert stackoverflow.com
